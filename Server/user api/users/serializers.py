5d14658accdc18cc6e2eb915056f602b1f0aec54
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-06-16 21:12:39 +0600

    Delete Server/user api directory

--- Server/user api/users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class UserSerializer(serializers.Serializer):
-    _id = serializers.CharField(read_only=True)
-    username = serializers.CharField()
-    email = serializers.EmailField()
-    bio = serializers.CharField(allow_blank=True, required=False)
-    avatar = serializers.CharField(allow_blank=True, required=False)
-    password = serializers.CharField(write_only=True)
