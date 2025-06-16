5b7f53bb9cdf8948cef1b168556546c5fe9a65c1
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-06-16 21:24:52 +0600

    Updated views.py in users app

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -24,30 +24,30 @@ class LoginUser(APIView):
             return Response(user.to_json())
         return Response({"error": "Invalid credentials"}, status=400)
 
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-from django.utils.html import escape
-import json
-from .models import User
-from django.contrib.auth.hashers import make_password
+# from django.views.decorators.csrf import csrf_exempt
+# from django.http import HttpResponse
+# from django.utils.html import escape
+# import json
+# from .models import User
+# from django.contrib.auth.hashers import make_password
 
-@csrf_exempt
-def register_form(request):
-    if request.method == 'POST':
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        password = make_password(request.POST.get("password"))
-        if User.objects(username=username):
-            return HttpResponse("Username already exists")
-        User(username=username, email=email, password=password).save()
-        return HttpResponse("User registered successfully")
+# @csrf_exempt
+# def register_form(request):
+#     if request.method == 'POST':
+#         username = request.POST.get("username")
+#         email = request.POST.get("email")
+#         password = make_password(request.POST.get("password"))
+#         if User.objects(username=username):
+#             return HttpResponse("Username already exists")
+#         User(username=username, email=email, password=password).save()
+#         return HttpResponse("User registered successfully")
 
-    return HttpResponse("""
-    <h2>Register User</h2>
-    <form method="post">
-        <label>Username: <input name="username"></label><br><br>
-        <label>Email: <input name="email" type="email"></label><br><br>
-        <label>Password: <input name="password" type="password"></label><br><br>
-        <button type="submit">Register</button>
-    </form>
-    """)
+#     return HttpResponse("""
+#     <h2>Register User</h2>
+#     <form method="post">
+#         <label>Username: <input name="username"></label><br><br>
+#         <label>Email: <input name="email" type="email"></label><br><br>
+#         <label>Password: <input name="password" type="password"></label><br><br>
+#         <button type="submit">Register</button>
+#     </form>
+#     """)
