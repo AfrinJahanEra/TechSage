8f12fb34fb790d465f35b56feb7aaf761362f6a9
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-06-17 21:24:06 +0600

    added lie/dislike in comment urls

--- Server/comments/urls.py
+++ Server/comments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostComment, GetComments
+from .views import PostComment, GetComments, LikeComment, DislikeComment
 
 urlpatterns = [
     path('post/', PostComment.as_view()),
     path('blog/<str:blog_id>/', GetComments.as_view()),
+    path('<str:comment_id>/like/', LikeComment.as_view()),
+    path('<str:comment_id>/dislike/', DislikeComment.as_view()),
 ]
