008f704d886e65d7d41730f6e4f6980afa3e4a75
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-06-20 15:05:58 +0600

    Updated urls.py

--- Server/techsage/urls.py
+++ Server/techsage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from users.views import RegisterUser, LoginUser
-from blogs.views import CreateBlog, ListBlogs
+from blogs.views import CreateBlog, ListBlogs, UpdateBlog, GetBlogVersions, RevertBlogVersion
 
 from django.http import FileResponse
 import os
@@ -14,4 +14,7 @@ urlpatterns = [
     path('create-blog/', CreateBlog.as_view()),
     path('blogs/', ListBlogs.as_view()),
     path('comments/', include('comments.urls')),
+    path('update-blog/<str:blog_id>/', UpdateBlog.as_view()),
+    path('blog-versions/<str:blog_id>/', GetBlogVersions.as_view()),
+    path('revert-blog/<str:blog_id>/<int:version_number>/', RevertBlogVersion.as_view()),
 ]
