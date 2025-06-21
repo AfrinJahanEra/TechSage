e907ad188e7d4922d245aeccaa5a77652d525c1e
era97 <afrinjahanera03@gmail.com>
2025-06-21 11:09:17 +0600

    responsive deisgn

--- Client/src/pages/AllBlogs.jsx
+++ Client/src/pages/AllBlogs.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import SearchForm from '../components/SearchForm';
 
 const AllBlogs = () => {
   const navigate = useNavigate();
