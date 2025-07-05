d2a338a669f2ebdb6fe5d24a5546d190964ef4a1
era97 <afrinjahanera03@gmail.com>
2025-07-05 18:03:34 +0600

    url added blogs endpoints

--- Server/techsage/urls.py
+++ Server/techsage/urls.py
@@ -12,7 +12,8 @@ from blogs.views import (
     UpdateBlog, GetBlogVersions, 
     RevertBlogVersion, DeleteBlog,
     ModeratorDeleteBlog, VoteBlog, 
-    BlogSearch
+    BlogSearch, PublishBlog, UnpublishBlog,
+    SaveAsDraft, RestoreBlog, AddAuthorToBlog
 )
 
 urlpatterns = [
@@ -21,17 +22,35 @@ urlpatterns = [
     path('user/<str:username>/', UserProfile.as_view(), name='user-profile'),
     path('user/<str:username>/saved-blogs/', SavedBlogsAPI.as_view(), name='saved-blogs'),
     path('search/', UserSearch.as_view(), name='user-search'),
-    # Blog URLs - specific endpoints first
+
     path('blogs/', ListBlogs.as_view()),
     path('blogs/create/', CreateBlog.as_view()),
-    path('blogs/search/', BlogSearch.as_view(), name='blog-search'),  # Moved before dynamic URLs
-    path('blogs/update/<str:blog_id>/', UpdateBlog.as_view()),
+    
+    path('blogs/publish/<str:blog_id>/', PublishBlog.as_view()),
+    path('blogs/unpublish/<str:blog_id>/', UnpublishBlog.as_view()),
+    path('blogs/draft/<str:blog_id>/', SaveAsDraft.as_view()),
+    path('blogs/restore/<str:blog_id>/', RestoreBlog.as_view()),
+    
+    path('blogs/add-author/<str:blog_id>/', AddAuthorToBlog.as_view()),
+    
+    path('blogs/search/', BlogSearch.as_view(), name='blog-search'),
+    
     path('blogs/versions/<str:blog_id>/', GetBlogVersions.as_view()),
     path('blogs/revert/<str:blog_id>/<int:version_number>/', RevertBlogVersion.as_view()),
+    
+    path('blogs/update/<str:blog_id>/', UpdateBlog.as_view()),
+    
     path('blogs/delete/<str:blog_id>/', DeleteBlog.as_view()),
     path('blogs/mod/delete/<str:blog_id>/', ModeratorDeleteBlog.as_view()),
+    
     path('blogs/vote/<str:blog_id>/', VoteBlog.as_view()),
-    path('blogs/<str:blog_id>/', GetBlog.as_view()),  # Moved to last
+    
+    path('blogs/<str:blog_id>/', GetBlog.as_view()),
+
+    
+
+    
+
 
     path('comments/', include('comments.urls')),
     path('reports/', include('reports.urls')),
