9f090334a07405d13debdd0f88b1ec263bdd137e
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-07-31 20:33:00 +0600

    Updated comments/urls.py
    added path for get comments by blog url

--- Server/comments/urls.py
+++ Server/comments/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<str:comment_id>/delete/', DeleteComment.as_view()),
     path('all/', GetAllComments.as_view()),  # New endpoint
     path('<str:comment_id>/review/', ReviewComment.as_view()),
+    path('blog/<str:blog_id>/', GetCommentsByBlog.as_view(), name='comments-by-blog'),
 ]
