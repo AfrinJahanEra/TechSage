87659c992552c2c20d2ce14ddbc5dda0f8d349c2
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-07-31 20:33:28 +0600

    Updated comments/urls.py
    commented out getcomment method's path

--- Server/comments/urls.py
+++ Server/comments/urls.py
@@ -4,7 +4,7 @@ from .views import PostComment, GetComments, LikeComment, DeleteComment, GetAllC
 
 urlpatterns = [
     path('post/', PostComment.as_view()),
-    path('blog/<str:blog_id>/', GetComments.as_view()),
+    # path('blog/<str:blog_id>/', GetComments.as_view()),
     path('<str:comment_id>/like/', LikeComment.as_view()),
     path('<str:comment_id>/delete/', DeleteComment.as_view()),
     path('all/', GetAllComments.as_view()),  # New endpoint
