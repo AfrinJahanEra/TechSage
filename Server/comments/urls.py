0268c3e70921c5693b49f5d0070e76d1ffdd15ec
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-08-01 15:25:58 +0600

    Updated comments/urls.py
    reverted to previous get comments path

--- Server/comments/urls.py
+++ Server/comments/urls.py
@@ -4,10 +4,10 @@ from .views import PostComment, GetComments, LikeComment, DeleteComment, GetAllC
 
 urlpatterns = [
     path('post/', PostComment.as_view()),
-    # path('blog/<str:blog_id>/', GetComments.as_view()),
+    path('blog/<str:blog_id>/', GetComments.as_view()),
     path('<str:comment_id>/like/', LikeComment.as_view()),
     path('<str:comment_id>/delete/', DeleteComment.as_view()),
     path('all/', GetAllComments.as_view()),  # New endpoint
     path('<str:comment_id>/review/', ReviewComment.as_view()),
-    path('blog/<str:blog_id>/', GetCommentsByBlog.as_view(), name='comments-by-blog'),
+    # path('blog/<str:blog_id>/', GetCommentsByBlog.as_view(), name='comments-by-blog'),
 ]
