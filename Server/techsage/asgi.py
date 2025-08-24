d432a99dd092c3118a7b87227e82a5e5936494d3
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-08-24 15:53:58 +0600

    added blogs/routing.py in asgi.py

--- Server/techsage/asgi.py
+++ Server/techsage/asgi.py
@@ -3,6 +3,7 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import comments.routing
+import blogs.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techsage.settings')
 django.setup()
@@ -10,6 +11,7 @@ django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": URLRouter(
-        comments.routing.websocket_urlpatterns
+        comments.routing.websocket_urlpatterns + 
+        blogs.routing.websocket_urlpatterns
     ),
 })
